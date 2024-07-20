aef01bbeef0586380c413717b158c4d618548105
Abdelrhman Momtaz <140316365+Abdelrhmanmomtaz58@users.noreply.github.com>
2024-07-20 10:49:47 +0300

    update order Status

--- app/src/main/java/com/momtaz/myapplication/activity/OderDetailsActivity.kt
+++ app/src/main/java/com/momtaz/myapplication/activity/OderDetailsActivity.kt
@@ -1,8 +1,14 @@
 package com.momtaz.myapplication.activity
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.firestore.FirebaseFirestore
 import com.momtaz.myapplication.R
 import com.momtaz.myapplication.adapter.CartOrderProductAdapter
 import com.momtaz.myapplication.data.CartProduct
@@ -12,6 +18,7 @@ import com.momtaz.myapplication.databinding.ActivityOderDetailsBinding
 class OderDetailsActivity : AppCompatActivity() {
     private lateinit var binding: ActivityOderDetailsBinding
     private lateinit var adapterCart: CartOrderProductAdapter
+    private lateinit var updateStatus: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityOderDetailsBinding.inflate(layoutInflater)
@@ -33,14 +40,60 @@ class OderDetailsActivity : AppCompatActivity() {
                 }
 
             }
-            val cartProduct: List<CartProduct> = order.products
 
 
         }
         if (order != null) {
             setupRecyclerView(order.products)
+            editOrderStatus(order)
         }
+
     }
+
+    private fun editOrderStatus(order: Order) {
+        binding.edit.setOnClickListener {
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("Edit order status")
+            val customLayout: View = layoutInflater.inflate(R.layout.custom_alert_dialog, null)
+            builder.setView(customLayout)
+            builder.setPositiveButton("Ok") { dialog: DialogInterface?, which: Int ->
+                val editText: EditText = customLayout.findViewById(R.id.edStatus)
+                if (editText.text != null) {
+                    updateStatus = editText.text.toString()
+                    updateStatus(updateStatus, order)
+                } else {
+                    Toast.makeText(this, "Enter new Status", Toast.LENGTH_LONG).show()
+                }
+            }
+            val dialog = builder.create()
+            dialog.show()
+        }
+
+    }
+
+    private fun updateStatus(updateStatus: String, order: Order) {
+        val db = FirebaseFirestore.getInstance()
+        val oderRef = db.collection("user").document(order.userid).collection("orders")
+        oderRef.whereEqualTo("orderId", order.orderId)
+            .get()
+            .addOnSuccessListener { querySnapshot ->
+                if (!querySnapshot.isEmpty) {
+                    for (document in querySnapshot.documents) {
+                        oderRef.document(document.id).update("orderStatus", updateStatus)
+                            .addOnSuccessListener {
+                                binding.tvOrderStatus.text = updateStatus
+                            }.addOnFailureListener {
+                                Toast.makeText(this, it.message, Toast.LENGTH_LONG).show()
+                            }
+                    }
+                } else {
+                    Toast.makeText(this, "No matching order found.", Toast.LENGTH_LONG).show()
+                }
+            }.addOnFailureListener {
+                Toast.makeText(this, it.message, Toast.LENGTH_LONG).show()
+            }
+    }
+
 
     private fun setupRecyclerView(products: List<CartProduct>) {
         adapterCart = CartOrderProductAdapter(products, this)
